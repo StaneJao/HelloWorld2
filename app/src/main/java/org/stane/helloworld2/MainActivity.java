e7619b3107bc0a334f154ac03c763ddabd92ada1
StaneJao <stanimir.vasic@gmail.com>
2016-06-12 22:29:43 +0200

    Added two toasts

--- app/src/main/java/org/stane/helloworld2/MainActivity.java
+++ app/src/main/java/org/stane/helloworld2/MainActivity.java
@@ -2,6 +2,7 @@ package org.stane.helloworld2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
         // This is the layout file I am linking.
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "this is a message", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "this is another message", Toast.LENGTH_SHORT).show();
     }
 }
