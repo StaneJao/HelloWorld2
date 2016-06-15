0f7b7fcc35831c2d537d55b7d73092a8affd6e20
StaneJao <stanimir.vasic@gmail.com>
2016-06-15 12:48:39 +0200

    Added username, password & login button. Added onClick events for both buttons.

--- app/src/main/java/org/stane/helloworld2/MainActivity.java
+++ app/src/main/java/org/stane/helloworld2/MainActivity.java
@@ -1,10 +1,15 @@
 package org.stane.helloworld2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
         // This is the layout file I am linking.
         setContentView(R.layout.activity_main);
-        String message = getString(R.string.message);
-        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        textView = (TextView) findViewById(R.id.text_view);
+        editText = (EditText) findViewById(R.id.username);
+    }
+
+    /* Called when btnExit has been clicked.     */
+    public void btnExit_click(View Button){
+        Toast.makeText(MainActivity.this, R.string.btnExit_message, Toast.LENGTH_SHORT).show();
+    }
+
+    /* Called when btnLogin has been clicked.     */
+    public void btnLogin_click(View Button) {
+        Toast.makeText(MainActivity.this, "btnLogin was clicked", Toast.LENGTH_SHORT).show();
+        String name = editText.getText().toString();
+        textView.setText(String.format("Hello there, %s!", name));
     }
 }
